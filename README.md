52598768526cbb8221d66f50f21ec50fda1ed0cf
hite <iamhite@gmail.com>
2019-08-31 17:25:35 +0800

    change: 抽离实现和增加 md 说明

--- README.md
+++ README.md
@@ -1,3 +1,159 @@
-# SwiftUI-CSS
+#  The missing CSS-like module for SwiftUI
+> Check out the [example project using SwiftUI-CSS](https://github.com/hite/SwiftUI-CSS_example)
 
-A description of this package.
+The SwiftUI is a great UI development framework for the iOS app. After I wrote some to-be-released app with SwiftUI framework, I realized that I need a solution to write more clear, simple, view-style-decoupled code with lots of custom style design.
+
+So here is **SwiftUI-CSS**. With **SwiftUI-CSS**, you can:
+
+## 1. write View-style-decoupled codes
+*View-style-decoupled* makes your source code more clear to read, easy to refactor like html with CSS support.
+
+### Without SwifUI-CSS:
+
+The codes to define View Structure blend into style-defined codes.
+
+``` swift
+             Image("image-swift")
+                 .resizable()
+                 .scaledToFit()
+                 .frame(width:100, height:100)
+                 .cornerRadius(10)
+                 .padding(EdgeInsets(top: 10, leading: 0, bottom: 15, trailing: 0))
+
+              Text("Swift")
+                 .font(.headline)
+                 .foregroundColor(Color(red: 0x33/0xff, green: 0x33/0xff, blue: 0x33/0xff))
+                 .padding(.bottom, 10)
+
+ 
+              Text("Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. for iOS, macOS, watchOS, tvOS, Linux, and z/OS. ")
+                 .font(.footnote)
+                 .padding(.horizontal, 10)
+                 .foregroundColor(NormalDescColor)
+                 .lineSpacing(2)
+                 .frame(minHeight: 100, maxHeight: .infinity)
+```
+### With SwifUI-CSS:
+
+1. We divide the previous into two parts. The first part is view structures with class name:
+
+``` swift
+            Image("image-swift")
+                .resizable()
+                .scaledToFit()
+                .addClassName(languageLogo_clsName)
+  
+            Text("Swift")
+                .addClassName(languageTitle_clsName)
+            
+     
+            Text("Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. for iOS, macOS, watchOS, tvOS, Linux, and z/OS. ")
+                .addClassName(languageDesc_clsName)
+```
+
+2. The another is style definition:
+``` swift
+let languageLogo_clsName = CSSStyle([
+    .width(100),
+    .height(100),
+    .cornerRadius(10),
+    .paddingTLBT(10, 0, 15,0)
+])
+
+let languageTitle_clsName = CSSStyle([
+    .font(.headline),
+    .foregroundColor(Color(red: 0x33/0xff, green: 0x33/0xff, blue: 0x33/0xff)),
+    .paddingEdges([.bottom], 10)
+])
+
+let languageDesc_clsName = CSSStyle([
+    .font(.footnote),
+    .paddingHorizontal(10),
+    .foregroundColor(NormalDescColor),
+    .lineSpacing(2),
+    .flexHeight(min: 50, max: .infinity)
+])
+```
+## 2. use module system for reuse or create a custom desing system.
+*module system* help to reuse some common style design across the whole app which can save you to write same codes everywhere or avoid to make some mistakes.
+
+### Without SwifUI-CSS:
+If you change the style of Text("28 October 2014"), you must change the style of Text("Objective-C,[7] Rust, Haskell, Ruby, Python, C#, CLU,[8] D[9]") too.
+```swift
+// in html5.swift
+                HStack() {
+                    Text("Initial release:")
+                        .font(Font.system(size: 14))
+                    
+                    Text("28 October 2014")
+                    .font(Font.system(size: 12))
+                    .foregroundColor(NormalDescColor)
+
+                }
+// in swift.swift
+                HStack(alignment: .top) {
+                    Text("Influenced by:")
+                        .font(Font.system(size: 14))
+                    
+                    Text("Objective-C,[7] Rust, Haskell, Ruby, Python, C#, CLU,[8] D[9]")
+                    .font(Font.system(size: 12))
+                    .foregroundColor(NormalDescColor)
+
+                }
+```
+### With SwiftUI-CSS
+You can change the definition of wikiDesc_clsName once for all.
+``` switfui
+let wikiDesc_clsName = CSSStyle([
+    .font(Font.system(size: 12)),
+    .foregroundColor(NormalDescColor)
+])
+
+// in html5.swift
+                HStack() {
+                    Text("Initial release:")
+                        .font(Font.system(size: 14))
+                    
+                    Text("28 October 2014")
+                    .addClassName(wikiDesc_clsName)
+
+                }
+// in swift.swift
+                HStack(alignment: .top) {
+                    Text("Influenced by:")
+                        .font(Font.system(size: 14))
+                    
+                    Text("Objective-C,[7] Rust, Haskell, Ruby, Python, C#, CLU,[8] D[9]")
+                    .addClassName(wikiDesc_clsName)
+
+                }
+```
+
+## the other benefits of using SwiftUI-CSS
+1. more easy to change a lot of styles when state change.
+``` switfui
+// without swiftui-css
+if festival == 'Christmas' {
+     Text("Welcome everyone!")
+     .font(.largeTitle)
+     .foreground(.white)
+     .background(.red)
+} else {
+        Text("Welcome everyone!")
+     .font(.title)
+     .foreground(.darkGray)
+     .background(.white)
+}
+
+// with
+Text("Welcome everyone!")
+.addClassName(fesitval == 'Christmas' ? chrismas_clsName: normal_clsName)
+```
+2. Maybe a reachable way to convert html+css codes  to swiftui source
+3. write less code, clear to tell parameters meanings. For example. 
+> `.frame(minHeight: 50, maxheight: .infinity` to `.flexHeight(min: 50, max: .infinity)`
+> `.padding(EdgeInset(top:10, leading: 15, bottom:0, trailing: 20)` to `.paddingTLBT(10,15,0,20)`
+4. (to be continued)
+
+
+
--- Sources/SwiftUI-CSS/SwiftUI_CSS.swift
+++ Sources/SwiftUI-CSS/SwiftUI_CSS.swift
@@ -1,3 +1,206 @@
-struct SwiftUI_CSS {
-    var text = "Hello, World!"
+//
+//  CSSStyle.swift
+//  SwiftUI_CSS
+//
+//  Created by liang on 2019/8/6.
+//  Copyright © 2019 liang. All rights reserved.
+//
+
+
+import SwiftUI
+import Foundation
+
+// https://mecid.github.io/2019/08/07/viewmodifiers-in-swiftui/
+extension View {
+    func eraseToAnyView() -> AnyView {
+        return AnyView(self)
+    }
+}
+
+enum CSSProperty {
+    case font(Font)
+    case foregroundColor(Color)
+    case backgroundColor(Color)
+    case lineLimit(Int)
+    case lineSpacing(CGFloat)
+    
+    case padding(CGFloat)
+    case paddingHorizontal(CGFloat)
+    case paddingVertical(CGFloat)
+    case paddingEdges(Edge.Set, CGFloat)
+    // TLBT = top + leading + bottom + trailing .It differs from css-style definition that is top, right, bottom, left
+    case paddingTLBT(CGFloat, CGFloat, CGFloat, CGFloat)
+    
+    case opacity(Double)
+    case shadow(color: Color, radius: CGFloat, x: CGFloat, y: CGFloat)
+    case border(color: Color, width: CGFloat)
+    case cornerRadius(_ radius: CGFloat)
+    // frame cluster
+    case frameAlignment(Alignment)
+    case width(CGFloat)
+    case height(CGFloat)
+    case frame(width: CGFloat?, height: CGFloat?, alignment: Alignment)
+    case flexWidth(min: CGFloat?, max: CGFloat?)
+    case flexHeight(min: CGFloat?, max: CGFloat?)
+
+    case position(x: CGFloat, y: CGFloat)
+    case offset(x: CGFloat, y: CGFloat)
+    case clipped(antialiased: Bool)
+    
+}
+
+extension CSSProperty {
+    
+}
+
+struct CSSStyle {
+    
+    fileprivate var properties: [CSSProperty]
+    
+    init(_ properties: [CSSProperty]) {
+        self.properties = properties
+    }
+    
+    func applyStyle(content: AnyView) -> AnyView {
+        return properties.reduce(content) { (newContent: AnyView, style: CSSProperty) -> AnyView in
+            switch style {
+                
+            case .font(let font):
+                return self.applyFont(content: newContent, font: font)
+            case .foregroundColor(let color):
+                return self.applyForegroundColor(content: newContent, color: color)
+            case .backgroundColor(let bgColor):
+                return self.applyBackgroundColor(content: newContent, color: bgColor)
+            case .lineLimit(let limit):
+                return self.applyLineLimit(content: newContent, limit: limit)
+            case .lineSpacing(let spacing):
+                return self.applyLineSpacing(content: newContent, spacing: spacing)
+            case .padding(let padding):
+                return self.applyPaddingByEdge(content: newContent, directions: [.all], padding: padding)
+            case .paddingHorizontal(let padding):
+                return self.applyPaddingByEdge(content: newContent, directions: [.horizontal], padding: padding)
+            case let .paddingEdges(edges, padding):
+                return self.applyPaddingByEdge(content: newContent, directions: edges, padding: padding)
+            case .paddingVertical(let padding):
+                return self.applyPaddingByEdge(content: newContent, directions: [.vertical], padding: padding)
+            case let .paddingTLBT(top, leading, bottom, trailing):
+                return self.applyPaddingAll(content: newContent, inset: .init(top: top, leading: leading, bottom: bottom, trailing: trailing))
+            case .opacity(let opacity):
+                return self.applyOpacity(content: newContent, opacity: opacity)
+            case let .shadow(color, radius, x, y):
+                return self.applyShadow(content: newContent, color: color, radius: radius, x: x, y: y)
+            case let .border(color, width):
+                return self.applyBorder(content: newContent, color: color, width: width)
+            case let .cornerRadius(width):
+                return self.applyCornerRadius(content: newContent, radius: width)
+            case .frameAlignment(let align):
+                return self.applyFrameAlignment(content: newContent, align: align)
+            case .width(let width):
+                return self.applyWidth(content: newContent, width: width)
+            case .height(let height):
+                return self.applyHeight(content: newContent, height: height)
+            case let .frame(width, height, alignment):
+                return self.applyFrame(content: newContent, width: width, height: height, alignment: alignment)
+            case let .flexWidth(minWidth, maxWidth):// the disadv
+                return self.applyFlexWidth(content: newContent, minWidth: minWidth, idealWidth: nil, maxWidth: maxWidth)
+            case let .flexHeight(minHeight, maxHeight):
+                return self.applyFlexHeight(content: newContent, minHeight: minHeight, idealHeight: nil, maxHeight: maxHeight)
+            case let .position(x, y):
+                return self.applyPosition(content: newContent, x: x, y: y)
+            case let .offset(x, y):
+                return self.applyOffset(content: newContent, x: x, y: y)
+            case .clipped(let antialiased):
+                return self.applyClipped(content: newContent, antialiased: antialiased)
+            }
+        }
+    }
+    
+    func applyForegroundColor(content: AnyView, color: Color) -> AnyView {
+        return content.foregroundColor(color).eraseToAnyView()
+    }
+    func applyFont(content: AnyView, font: Font?) -> AnyView {
+        return content.font(font).eraseToAnyView()
+    }
+    func applyBackgroundColor(content: AnyView, color: Color) -> AnyView {
+        return content.background(color).eraseToAnyView()
+    }
+    func applyLineLimit(content: AnyView, limit: Int) -> AnyView {
+        return content.lineLimit(limit).eraseToAnyView()
+    }
+    func applyLineSpacing(content: AnyView, spacing: CGFloat) -> AnyView {
+        return content.lineSpacing(spacing).eraseToAnyView()
+    }
+    func applyPaddingByEdge(content: AnyView, directions: Edge.Set, padding: CGFloat) -> AnyView {
+        return content.padding(directions, padding).eraseToAnyView()
+    }
+    func applyPaddingAll(content: AnyView, inset: EdgeInsets) -> AnyView {
+        return content.padding(inset).eraseToAnyView()
+    }
+    func applyOpacity(content: AnyView, opacity: Double) -> AnyView {
+        return content.opacity(opacity).eraseToAnyView()
+    }
+    func applyShadow(content: AnyView, color: Color, radius: CGFloat, x: CGFloat, y: CGFloat) -> AnyView {
+        return content.shadow(color: color, radius: radius, x: x, y: y).eraseToAnyView()
+    }
+    func applyBorder(content: AnyView, color: Color, width: CGFloat) -> AnyView {
+        return content.border(color, width: width).eraseToAnyView()
+    }
+    func applyCornerRadius(content: AnyView, radius: CGFloat) -> AnyView {
+        return content.cornerRadius(radius, antialiased: true).eraseToAnyView()
+    }
+    func applyFrameAlignment(content: AnyView, align: Alignment) -> AnyView {
+        return content.frame(alignment: align).eraseToAnyView()
+    }
+    func applyWidth(content: AnyView, width: CGFloat) -> AnyView {
+        return content.frame(width: width).eraseToAnyView()
+    }
+    func applyHeight(content: AnyView, height: CGFloat) -> AnyView {
+        return content.frame(height: height).eraseToAnyView()
+    }
+    func applyFrame(content: AnyView, width: CGFloat?, height: CGFloat?, alignment: Alignment) -> AnyView {
+        return content.frame(width: width, height: height, alignment: alignment).eraseToAnyView()
+    }
+    func applyFlexWidth(content: AnyView, minWidth: CGFloat?, idealWidth: CGFloat?, maxWidth: CGFloat?) -> AnyView {
+        return content.frame(minWidth: minWidth, idealWidth: idealWidth, maxWidth: maxWidth).eraseToAnyView()
+    }
+    func applyFlexHeight(content: AnyView, minHeight: CGFloat?, idealHeight: CGFloat?, maxHeight: CGFloat?) -> AnyView {
+        return content.frame(minHeight: minHeight, idealHeight: idealHeight, maxHeight: maxHeight).eraseToAnyView()
+    }
+    func applyPosition(content: AnyView, x: CGFloat, y: CGFloat) -> AnyView {
+        return content.position(x:x, y: y).eraseToAnyView()
+    }
+    func applyOffset(content: AnyView, x: CGFloat, y: CGFloat) -> AnyView {
+        return content.offset(x:x, y: y).eraseToAnyView()
+    }
+    func applyClipped(content: AnyView, antialiased: Bool) -> AnyView {
+        return content.clipped(antialiased: antialiased).eraseToAnyView()
+    }
+    
+}
+
+struct CSSStyleModifier: ViewModifier {
+    fileprivate let styleSheet: CSSStyle
+    
+    func body(content: _ViewModifier_Content<CSSStyleModifier>) -> some View {
+        
+        return styleSheet.applyStyle(content: content.eraseToAnyView())
+    }
+}
+
+
+extension View {
+
+    func addClassName(_ clsName: CSSStyle) -> some View {
+        ModifiedContent(content: self, modifier: CSSStyleModifier(styleSheet: clsName))
+    }
+    
+    func setStyle(_ properties: [CSSProperty]) -> some View {
+        ModifiedContent(content: self, modifier: CSSStyleModifier(styleSheet: CSSStyle(properties)))
+    }
+    
+    func changeClassName(_ clsName: CSSStyle) -> some View {
+        // 省略
+        return self
+    }
+
 }
