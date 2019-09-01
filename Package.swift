423c62fd4cdbc40ced133284491371730f56dad0
hite <iamhite@gmail.com>
2019-09-01 18:10:47 +0800

    change:  add 'sources' field value

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftUI-CSS",
-            dependencies: []),
+            path: "Sources/SwiftUI-CSS"
+        ),
         .testTarget(
             name: "SwiftUI-CSSTests",
             dependencies: ["SwiftUI-CSS"]),
