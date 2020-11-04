84c10e585ef4fa663db232c81c290ab5a5a9f207
wang_liang <wang_liang@corp.netease.com>
2020-11-04 11:31:39 +0800

    change: make responsive.debug public

--- Sources/SwiftUI_CSS/Responsive.swift
+++ Sources/SwiftUI_CSS/Responsive.swift
@@ -12,12 +12,12 @@ import SwiftUI
 public class Responsive: ObservableObject {
     /// For example. the size of view in sketch file designed for iPhone 6 should larger  on iPhone by 1.104 ( 414 / 375) times.
     /// so the factor = 1.104
-    @Published var factor: CGFloat
+    @Published public var factor: CGFloat
     
-    init(_ factor: CGFloat) {
+    public init(_ factor: CGFloat) {
         self.factor = factor
     }
-    convenience init(designSize: CGRect) {
+    public convenience init(designSize: CGRect) {
         var factor: CGFloat = 1
         if designSize.equalTo(.zero) {
             factor = 1
@@ -39,5 +39,9 @@ public class Responsive: ObservableObject {
     
     public func r(_ standardSize: CGFloat) -> CGFloat {
         return self.factor * standardSize
+    }
+    
+    public func debug() -> String {
+        return "<Responsive:\(Unmanaged.passUnretained(self).toOpaque()) factor=\(self.factor)>"
     }
 }
